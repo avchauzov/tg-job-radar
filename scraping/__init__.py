cee31b405d92d65a3afaba46f3cce35feb7d68d6
Andrew <avchauzov@gmail.com>
2024-08-30 18:41:41 +0800

    keywords.json handling errors

--- scraping/__init__.py
+++ scraping/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
+from utils.functions import load_json_into_dict, validate_keywords
+
 
 load_dotenv()
 
@@ -11,7 +13,12 @@ PHONE_NUMBER = os.getenv('PHONE_NUMBER')
 
 SESSION_NAME = 'main'
 
-KEYWORDS = ['ai', 'ml', 'data scientist']
+KEYWORDS = load_json_into_dict('./keywords.json').get('keywords', [])
+KEYWORDS = validate_keywords(KEYWORDS)
+
+if len(KEYWORDS) == 0:
+	raise "Keywords can't be loaded!"
+
 KEYWORDS_LENGTH = {key: key.count(' ') + 1 for key in KEYWORDS}
 MIN_NGRAM_LENGTH = min(list(KEYWORDS_LENGTH.values()))
 MAX_NGRAM_LENGTH = max(list(KEYWORDS_LENGTH.values()))
