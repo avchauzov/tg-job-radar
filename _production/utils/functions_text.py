d9485a0fc5e207933d60652549ca95e7e2845686
avchauzov <avchauzov@gmail.com>
2024-12-12 13:22:52 +0700

    refactor: update utility functions - Enhance text processing utilities - Improve Telegram API integration - Update common helper functions

--- _production/utils/functions_text.py
+++ _production/utils/functions_text.py
@@ -4,21 +4,25 @@ from urllib.parse import urlparse, urlunparse
 
 
 def clean_job_description(text):
-	try:
-		
-		if not isinstance(text, str):
-			logging.warning(f'Expected a string, but got {type(text)}. Returning input as-is.')
-			return text
-		
-		text_cleaned = text.replace('\n', ' ')
-		text_cleaned = re.sub(r'\s+', ' ', text_cleaned).strip()
-		
-		return text_cleaned
-	
-	except Exception as error:
-		logging.warning(f'Error cleaning job description: {error}')
-		return text
+    try:
+        if not isinstance(text, str):
+            logging.warning(
+                f"Expected a string, but got {type(text)}\n{text}\nReturning input as string."
+            )
+            return str(text)
 
+        text_cleaned = text.replace("\n", " ")
+        text_cleaned = re.sub(r"\s+", " ", text_cleaned).strip()
+
+        return text_cleaned
+
+    except Exception as error:
+        logging.warning(f"Error cleaning job description: {error}\n{text}")
+        return text
+
+# TODO: revise logging format
+# TODO: more 'raise' than skip
+# TODO: airflow: notifications to email
 
 def normalize_url(url):
 	try:
--- _production/utils/functions_tg_api.py
+++ _production/utils/functions_tg_api.py
@@ -9,6 +9,7 @@ def create_session_string(client, config, config_path):
 		with open(config_path, 'w') as file:
 			json.dump(config, file)
 		logging.info('Session string saved. Please rerun the script to use the new session string.')
+		
 	except IOError as error:
 		logging.error(f'Error writing session string to config: {error}')
 		raise
