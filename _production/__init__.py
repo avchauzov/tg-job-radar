b5034accdb431c71cc538407ec489f67451dd108
Andrew <avchauzov@gmail.com>
2024-12-13 22:55:46 +0800

    feat: Initialize project configuration and environment setup
    - Add environment variable configurations
    - Set up database connection parameters
    - Configure email settings
    - Add API keys and authentication settings
    - Define global constants and patterns

--- _production/__init__.py
+++ _production/__init__.py
@@ -7,22 +7,26 @@ load_dotenv()
 
 # Database configurations
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "job_search")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_USER = os.getenv("DB_USER", "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+
+SENDER_EMAIL = os.getenv("SENDER_EMAIL", "avchauzov.dev@gmail.com")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL", "avchauzov.dev@gmail.com")
+
+GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD", "qwerty")
 
 # API Keys and Authentication
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LLM_BASE_MODEL = "gpt-4-turbo-preview"
+LLM_BASE_MODEL = "gpt-4o-mini"
 
 # Email Configuration
 EMAIL_NOTIFICATION_CHUNK_SIZE = 32
 
 # Database Table Names
-RAW_DATA__TG_POSTS = "raw.tg_posts"
-STAGING_DATA__POSTS = "staging.posts"
-PROD_DATA__JOBS = "prod.jobs"
+RAW_DATA__TG_POSTS = "raw_data.tg_posts"
+STAGING_DATA__POSTS = "staging_data.posts"
+PROD_DATA__JOBS = "prod_data.jobs"
 
 # Constants
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -30,5 +34,3 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 # Regex patterns
 URL_EXTRACT_PATTERN = r"https?://[^\s()]+(?:\([\w\d]+\)|[^\s,()])*(?<![.,?!])"
 URL_REMOVAL_PATTERN = re.compile(r"(https?://\S+|www\.\S+)")
-
-# TODO: add tests
